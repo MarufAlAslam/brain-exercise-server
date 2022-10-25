49f22b523cf4e54c76fd5de4af4f62e74198244b
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-10-25 20:16:18 +0600

    api created

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const courses = require('./data.json')
 const port = 5000
 
 app.get('/', (req, res) => {
     res.send('Hey! Brain Exercise Server Running')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send('The course with the given ID was not found.')
+    res.send(course)
 })
 
 app.listen(port, () => {
